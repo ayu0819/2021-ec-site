4ed632911d228ec5bf6dfc872314794a1113aebf
ayu0819 <ayutateishi0819@gmail.com>
2021-04-26 16:35:21 +0900

    delete Files.

--- Desktop/デプロイ/deploy-ec-site/src/reducks/Contacts/actions.js
+++
@@ -1,7 +0,0 @@
-export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
-export const fetchProductsAction = (products) => {
-    return {
-        type: "FETCH_PRODUCTS",
-        payload: products
-    }
-}
--- Desktop/デプロイ/deploy-ec-site/src/reducks/Notification/selectors.js
+++
@@ -1,8 +0,0 @@
-import {createSelector} from 'reselect';
-
-const notificationsSelector = (state) => state.notifications;
-
-export const getNotifications = createSelector(
-    [notificationsSelector],
-    state => state.list
-);
