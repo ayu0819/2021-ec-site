2a7bab8c3977f1bce207708a9392f3af0730c286
ayu0819 <ayutateishi0819@gmail.com>
2021-04-26 16:34:33 +0900

    delete commit.

--- Desktop/new-開発中サイト/ec-site/src/reducks/Notification/actions.js
+++
@@ -1,7 +0,0 @@
-export const FETCH_NOTIFICATIONS = "FETCH_NOTIFICATIONS";
-export const fetchNotificationsAction = (notifications) => {
-    return {
-        type: "FETCH_NOTIFICATIONS",
-        payload: notifications
-    }
-};
